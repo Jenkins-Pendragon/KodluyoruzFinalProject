b51517b184ff7ad216dc532289e701ccb2af03a8
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-05 01:13:01 +0300

    Scene adjustments

---
+++ Assets/[Game]/Scripts/Editor/LevelFix.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+
+
+[InitializeOnLoad]
+public class LevelFix
+{    
+    static LevelFix()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+  
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            bool initCheck = true;
+            int sceneCount = SceneManager.sceneCount;            
+            string scenePath = "Assets/[Game]/Scenes/Init.unity";
+            //string scenePath = AssetDatabase.GetAssetPath(init);
+
+
+            for (int i = 0; i < sceneCount; i++)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+                if (!scene.name.Contains("Init"))
+                {
+                    initCheck = false;                    
+                    break;
+                }
+            }
+
+            if (!initCheck)
+            {                
+                SetPlayModeStartScene(scenePath);
+            }
+        }
+    }
+
+    private static void SetPlayModeStartScene(string scenePath)
+    {
+        SceneAsset myWantedStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
+        if (myWantedStartScene != null)
+        {
+            EditorSceneManager.playModeStartScene = myWantedStartScene;
+        }
+        else Debug.Log("Could not find Scene " + scenePath);
+    }
+
+
+}
---
+++ Assets/[Game]/Scripts/Managers/InitManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class InitManager : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        //Init Game Here
+        yield return SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+        yield return SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneAt(2));        
+        Destroy(gameObject);
+    } 
+
+    
+}
