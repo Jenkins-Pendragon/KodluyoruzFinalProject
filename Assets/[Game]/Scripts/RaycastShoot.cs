a8ce0c74c8e6f155dffd7a691c1d36c3a83969b6
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-05 15:58:37 +0300

    Add RaycastShoot script

--- Assets/[Game]/Scripts/RaycastShoot.cs
+++ Assets/[Game]/Scripts/RaycastShoot.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class RaycastShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera raycastCam;
+    public Transform handEndPoint;
+    private LineRenderer laserLine;
+    private float rayRange = 100f;
+    
     void Start()
-    {
-        
+    {        
+        laserLine = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
+    
     void Update()
-    {
-        
+    {    
+        if (Input.GetMouseButtonDown(0))
+        {
+            laserLine.enabled = true;
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+            laserLine.enabled = false;
+        }
+
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 rayOrigin = raycastCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+            RaycastHit hit;
+            laserLine.SetPosition(0, handEndPoint.position);
+            if (Physics.Raycast(rayOrigin, raycastCam.transform.forward, out hit, rayRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (raycastCam.transform.forward * rayRange));
+            }
+        }
     }
 }
