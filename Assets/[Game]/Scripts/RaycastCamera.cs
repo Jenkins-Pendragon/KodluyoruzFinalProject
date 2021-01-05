f09daa7160f2dfacae4dc263311d162d44c635a2
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-05 15:16:05 +0300

    Add RaycastCamera script

---
+++ Assets/[Game]/Scripts/RaycastCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastCamera : MonoBehaviour
+{
+    public float mouseSensivity = 100f;    
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+        xRotation = Mathf.Clamp(xRotation, -25f, 25f);
+        yRotation = Mathf.Clamp(yRotation, -18f, 18f);
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+    }
+}
