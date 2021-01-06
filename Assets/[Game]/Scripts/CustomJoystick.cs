26e734aa0f451b61327a35589e55cbcd34086216
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-06 16:55:11 +0300

    Fix CustomJoyStick

--- Assets/[Game]/Scripts/CustomJoystick.cs
+++ Assets/[Game]/Scripts/CustomJoystick.cs
@@ -17,25 +17,21 @@ public class CustomJoystick : Joystick
     }
 
     public override void OnPointerDown(PointerEventData eventData)
-    {
-        EventManager.OnPointerUp.Invoke(eventData);
+    {        
         background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
         background.gameObject.SetActive(true);
         base.OnPointerDown(eventData);
     }
 
     public override void OnPointerUp(PointerEventData eventData)
-    {
-        EventManager.OnPointerUp.Invoke(eventData);
+    {        
         background.gameObject.SetActive(false);
         base.OnPointerUp(eventData);
     }
 
     public override void OnDrag(PointerEventData eventData)
-    {
-        EventManager.OnDrag.Invoke(eventData);
-        base.OnDrag(eventData);
-        
+    {        
+        base.OnDrag(eventData);       
     }
 
     protected override void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
