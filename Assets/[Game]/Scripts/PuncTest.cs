fa1b2fd61df104a345a1dc8efb85ba306092157a
enkalados <yakup_mknc@hotmail.com>
2021-01-06 21:36:40 +0300

    punchtest

---
+++ Assets/[Game]/Scripts/PuncTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuncTest : MonoBehaviour
+{
+    [SerializeField] Animator animc;
+    private void Start()
+    {
+        animc = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("aaaaaa");
+        animc.SetBool("Running", false);
+        animc.SetBool("Punching", true);
+    }
+}
