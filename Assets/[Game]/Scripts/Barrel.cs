c6d3820b911fd7477668f55b4998b5fdf49f5275
onurApphic <o.ozdemir@apphicgames.com>
2021-01-07 21:12:00 +0300

    barrelcs edit

--- Assets/[Game]/Scripts/Barrel.cs
+++ Assets/[Game]/Scripts/Barrel.cs
@@ -4,7 +4,7 @@ public class Barrel : MonoBehaviour
 {
     public float radius = 5.0f;
     public float power = 20.0f;
-    public GameObject blastEffectPrefab;
+    public GameObject blastEffectPrefab; // Bunu daha sonra static instance'i olan bir classa atıp ordan çekelim // Örneğin GameData classı olabilir
 
     private void OnMouseDown()
     {
@@ -12,7 +12,7 @@ public class Barrel : MonoBehaviour
     }
     public void OnRelation()
     {
-        Instantiate(blastEffectPrefab);
+        Instantiate(blastEffectPrefab,transform.position,Quaternion.identity);
         Vector3 explosionPos = transform.position;
         Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
         foreach (Collider collider in colliders)
@@ -20,7 +20,7 @@ public class Barrel : MonoBehaviour
             Rigidbody myRigidbody = collider.GetComponent<Rigidbody>();
             if (myRigidbody != null)
             {
-                myRigidbody.AddExplosionForce(power, explosionPos, radius, 3.0f, ForceMode.Impulse);
+                myRigidbody.AddExplosionForce(power, explosionPos, radius, 3.0f , ForceMode.Impulse);
             }
         }
         this.gameObject.SetActive(false);
