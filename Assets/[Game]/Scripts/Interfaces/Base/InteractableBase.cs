ff1d8a1e49b01519b625bdd5eabee380bfa75197
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-08 18:56:27 +0300

    Update InteractableBase (Outline enable/Disable)

--- Assets/[Game]/Scripts/Interfaces/Base/InteractableBase.cs
+++ Assets/[Game]/Scripts/Interfaces/Base/InteractableBase.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using DG.Tweening;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(OutlineShader))]
 public abstract class InteractableBase : MonoBehaviour, IInteractable
 {
     public bool IsInteractable { get; set; }
     public bool IsKillable { get; set; }
     private Rigidbody rb;
     public Rigidbody RigidbodyObj { get { return (rb == null) ? rb = GetComponentInChildren<Rigidbody>() : rb; } }
-  
+    private OutlineShader outline;
+    public OutlineShader Outline { get { return (outline == null) ? outline = GetComponentInChildren<OutlineShader>() : outline; } }
+
     private readonly float throwForce = 50f;
     private readonly float tweenDelay = 0.3f;
     private void Start()
@@ -22,14 +25,16 @@ public abstract class InteractableBase : MonoBehaviour, IInteractable
     {
         IsInteractable = true;
         IsKillable = false;
+        Outline.enabled = false;
+        Outline.Initiliaze(Color.yellow, 8f, OutlineShader.Mode.OutlineVisible);
     }
 
     public virtual void OnInteractStart(Transform parent, Transform destination)
     {
         IsInteractable = false;
-        if (GetComponent<OutlineShader>() == null)
-        {
-            transform.gameObject.AddComponent<OutlineShader>().Initiliaze(Color.yellow, 8f, OutlineShader.Mode.OutlineVisible);
+        if (Outline.enabled == false)
+        {            
+            Outline.enabled = true;
         }        
         transform.parent = parent;
         RigidbodyObj.isKinematic = true;
@@ -39,11 +44,10 @@ public abstract class InteractableBase : MonoBehaviour, IInteractable
 
     public virtual void OnInteractEnd(Transform forceDirection)
     {
-        transform.DOKill();
-        OutlineShader outlineShader = GetComponent<OutlineShader>();
-        if (GetComponent<OutlineShader>() != null)
+        transform.DOKill();        
+        if (Outline.enabled == true)
         {
-            Destroy(outlineShader);
+            Outline.enabled = false;
         }
         gameObject.transform.parent = null;
         RigidbodyObj.isKinematic = false;
