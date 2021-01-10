aade60310cfe205a9f82b662b03c26cc4fd9a4d7
enkalados <yakup_mknc@hotmail.com>
2021-01-11 00:00:17 +0300

    Cannon added

---
+++ Assets/[Game]/Scripts/Objects/Cannon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Cannon : MonoBehaviour
+{
+    public bool canFire;
+    public Ease myEase;
+    [SerializeField] GameObject cannonBall;
+    public IEnumerator StartFire()
+    {
+        while (canFire)
+        {
+            yield return new WaitForSeconds(2f);
+            Debug.Log("fire!");
+            transform.DOLookAt(PlayerTransfomStreamer.Instance.transform.position, 0.5f).OnComplete(() =>
+            {
+                GameObject clone = Instantiate(cannonBall, transform.position, Quaternion.identity);
+                clone.transform.DOJump(PlayerTransfomStreamer.Instance.transform.position+Vector3.forward * 5, 6, 1, 1.5f).SetEase(myEase);
+            });
+        }
+    }
+}
---
+++ Assets/[Game]/Scripts/Player/PlayerTransfomStreamer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTransfomStreamer : MonoBehaviour
+{
+    public static PlayerTransfomStreamer Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Cannon cannon = other.GetComponent<Cannon>();
+        if (cannon!=null)
+        {
+            cannon.canFire = true;
+            StartCoroutine(cannon.StartFire());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Cannon cannon = other.GetComponent<Cannon>();
+        if (cannon != null)
+        {
+            cannon.canFire = false;
+            StopCoroutine(cannon.StartFire());
+        }
+    }
+}
