26fc20f6cf70da422f5ef474a4a4952ee45fbfa9
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-10 18:27:07 +0300

    Added Ragdoll Controller script

---
+++ Assets/[Game]/Scripts/Objects/RagdollController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    public List<Rigidbody> rigidbodies;
+    public List<Collider> colliders;
+    public Rigidbody mainRigidbody;
+    public Collider mainCollider;
+
+    public void ActivateRagdoll() 
+    {
+    
+    }
+
+    public void DisableRagdoll() 
+    {
+        
+    }
+}
