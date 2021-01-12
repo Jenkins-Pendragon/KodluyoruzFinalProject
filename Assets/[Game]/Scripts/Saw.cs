22b91701729f21f2a6252b8f0e329e18d8d5f237
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-12 15:00:33 +0300

    Add Slice scripts

---
+++ Assets/[Game]/Scripts/Saw.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+
+public class Saw : MonoBehaviour
+{
+    public Material material;
+    public LayerMask mask;
+
+
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.position += new Vector3(h, v, 0) * 0.5f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Collider[] colliders = Physics.OverlapBox(transform.position, new Vector3(1f, 0.1f, 0.1f), transform.rotation, mask);
+
+            foreach (Collider item in colliders)
+            {
+                SliceChibi sliceChibi = item.GetComponent<SliceChibi>();
+                if (sliceChibi != null)
+                {
+                    GameObject obj = sliceChibi.Slice();
+                    SlicedHull slicedHull = Slice(obj.GetComponent<Collider>().gameObject, material);
+                    GameObject upperHull = slicedHull.CreateUpperHull(obj.gameObject, material);
+                    GameObject lowerHull = slicedHull.CreateLowerHull(obj.gameObject, material);
+                    AddComponents(upperHull);
+                    AddComponents(lowerHull);
+                    Destroy(obj.gameObject);
+                }
+                else
+                {
+                    SlicedHull slicedHull = Slice(item.GetComponent<Collider>().gameObject, material);
+                    GameObject upperHull = slicedHull.CreateUpperHull(item.gameObject, material);
+                    GameObject lowerHull = slicedHull.CreateLowerHull(item.gameObject, material);
+                    AddComponents(upperHull);
+                    AddComponents(lowerHull);
+                    Destroy(item.gameObject);
+                }
+
+            }
+        }
+    }
+
+    private SlicedHull Slice(GameObject obj, Material material)
+    {
+        return obj.Slice(transform.position, transform.up, material);
+    }
+
+    private void AddComponents(GameObject obj)
+    {
+        obj.AddComponent<MeshCollider>().convex = true;
+        Rigidbody rb = obj.AddComponent<Rigidbody>();
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.AddExplosionForce(100, obj.transform.position, 20);
+    }
+}
---
+++ Assets/[Game]/Scripts/SliceChibi.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceChibi : MonoBehaviour
+{
+    public Mesh[] meshList;
+    public GameObject cube;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    private Animator playerAnimator;
+    private Mesh mesh;
+
+
+    private void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Slice();
+        }
+    }
+
+    public GameObject Slice()
+    {
+        AnimatorClipInfo[] animationClip = playerAnimator.GetCurrentAnimatorClipInfo(0);
+        int frameNum = (int)(animationClip[0].weight * (animationClip[0].clip.length * animationClip[0].clip.frameRate));
+        int currentFrame = (int)(playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime * (frameNum)) % frameNum;
+        if (currentFrame >= meshList.Length) currentFrame = 0;
+        GameObject obj = Instantiate(cube, transform.position, Quaternion.identity);
+        obj.GetComponent<MeshFilter>().mesh = meshList[currentFrame];
+        Destroy(gameObject);
+        return obj;
+    }
+
+    public GameObject Slice2()
+    {
+        mesh = new Mesh();
+        mesh = skinnedMeshRenderer.sharedMesh;
+        mesh.RecalculateNormals();
+        GameObject obj = Instantiate(cube, transform.position, Quaternion.identity);
+        cube.GetComponent<MeshFilter>().mesh = mesh;
+        Destroy(gameObject);
+        return obj;
+    }
+}
