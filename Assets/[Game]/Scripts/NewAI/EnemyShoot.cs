bbe9e5c561f507e1075c56c35d148319d2cd8b43
Utku <utku.bagdas@gmail.com>
2021-01-12 18:29:29 +0300

    Added EnemyShoot(incomplete)

---
+++ Assets/[Game]/Scripts/NewAI/EnemyShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    [SerializeField] private Transform Bullet;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            EnemyShooting();
+        }
+    }
+
+
+
+    private void EnemyShooting()
+    {
+        Instantiate(Bullet, transform.position, Quaternion.identity);
+    }
+
+}
