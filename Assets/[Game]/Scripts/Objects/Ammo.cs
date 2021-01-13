3537586ea3b2ec6563fabc558d81081e18f4d5c2
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-14 02:39:55 +0300

    Import weapon pack

--- Assets/[Game]/Scripts/Objects/Ammo.cs
+++ Assets/[Game]/Scripts/Objects/Ammo.cs
@@ -7,16 +7,11 @@ public class Ammo : Prop
     
 {
     private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.gameObject.name);
+    {        
         if (IsInteractable)
         {
-            IInteractable interactable = collision.gameObject.GetComponent<IInteractable>();
-            if (interactable != null)
-            {
-                transform.DOKill();
-                RigidbodyObj.useGravity = true;
-            }
+            transform.DOKill();
+            RigidbodyObj.useGravity = true;
         }
-    }
+    }  
 }
