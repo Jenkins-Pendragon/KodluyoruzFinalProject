d252f6779e50c4496d50d42f9e0d932341135a82
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-15 01:52:31 +0300

    Fixed Ground Collisions

--- Assets/[Game]/Scripts/Objects/Ammo.cs
+++ Assets/[Game]/Scripts/Objects/Ammo.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class Ammo : Prop
-    
+public class Ammo : Prop, IPlayerKillable    
 {
-    private void OnCollisionEnter(Collision collision)
-    {        
+    public bool IsDeadly { get; set;}
+
+    private void Awake()
+    {
+        IsDeadly = true;
+    }
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        base.OnCollisionEnter(collision);
         if (IsInteractable)
-        {
+        {            
             transform.DOKill();
             RigidbodyObj.useGravity = true;
+            IsDeadly = false;
         }
-    }  
+    }
 }
