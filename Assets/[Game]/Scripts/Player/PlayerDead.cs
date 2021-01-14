a8b4ca7831b1c7f205496431084493140110ffc8
Kasim13 <54179910+Kasim13@users.noreply.github.com>
2021-01-15 01:27:31 +0300

    Dead
    Player ölümü.

---
+++ Assets/[Game]/Scripts/Player/PlayerDead.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerDead : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            transform.DORotate(
+            new Vector3(0, 0, -80), 1.5f, RotateMode.FastBeyond360);
+        }
+    }
+}
