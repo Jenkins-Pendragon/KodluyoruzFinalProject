71616abed43407375bb1f16f4021318b7ccb970d
enkalados <yakup_mknc@hotmail.com>
2021-01-16 00:03:26 +0300

    CameraFollow

---
+++ Assets/[Game]/Scripts/FinalCamFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FinalCamFollow : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    void Start()
+    {
+        Tweener tweener = transform.DOMove(target.transform.position, 10).SetSpeedBased();
+        tweener.OnUpdate(() => tweener.ChangeEndValue(target.transform.position + Vector3.back*5 +Vector3.up*2, true));
+    }
+
+}
