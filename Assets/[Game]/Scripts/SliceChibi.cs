a722908f5f8c1902cff64c4f22e6585127c0a5bc
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-15 20:41:10 +0300

    Update SliceChibi.cs

--- Assets/[Game]/Scripts/SliceChibi.cs
+++ Assets/[Game]/Scripts/SliceChibi.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SliceChibi : MonoBehaviour
 {
-    public Mesh[] meshList;
+    public List<Mesh> runningMeshList;
+    public List<Mesh> shootingMeshList;
+    public List<Mesh> tauntMeshList;
     public GameObject cube;
     public SkinnedMeshRenderer skinnedMeshRenderer;
     public Transform spawnPoint;
@@ -17,11 +19,18 @@ public class SliceChibi : MonoBehaviour
 
     public GameObject Slice()
     {
+        List<Mesh> meshList;
         AnimatorClipInfo[] animationClip = playerAnimator.GetCurrentAnimatorClipInfo(0);
-        Debug.Log(animationClip[0].clip.name);
+        string animationName = animationClip[0].clip.name;
         int frameNum = (int)(animationClip[0].weight * (animationClip[0].clip.length * animationClip[0].clip.frameRate));
         int currentFrame = (int)(playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime * (frameNum)) % frameNum;
-        if (currentFrame >= meshList.Length) currentFrame = 0;
+
+        if (animationName == "Running") meshList = runningMeshList;
+        else if (animationName == "Shooting" || animationName == "DefaultStateShooting") meshList = shootingMeshList;
+        else if(animationName == "Taunt") meshList = tauntMeshList;
+        else meshList = runningMeshList;
+
+        if (currentFrame >= meshList.Count) currentFrame = 0;
         GameObject obj = Instantiate(cube, spawnPoint.position, transform.rotation);
         obj.GetComponent<MeshFilter>().mesh = meshList[currentFrame];
         gameObject.SetActive(false);
