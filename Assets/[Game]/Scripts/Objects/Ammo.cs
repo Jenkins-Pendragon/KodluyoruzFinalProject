7f0d46923bb8b5a6d5a6997e2fe985e3db73dd09
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-15 14:29:04 +0300

    Added TrailRenderer for Bullet prefab

--- Assets/[Game]/Scripts/Objects/Ammo.cs
+++ Assets/[Game]/Scripts/Objects/Ammo.cs
@@ -6,10 +6,18 @@ using DG.Tweening;
 public class Ammo : Prop, IPlayerKillable    
 {
     public bool IsDeadly { get; set;}
+    private TrailRenderer trailRenderer;
+    public TrailRenderer TrailRenderer { get { return (trailRenderer == null) ? trailRenderer = GetComponent<TrailRenderer>() : trailRenderer; } }
 
     private void Awake()
     {
         IsDeadly = true;
+    }
+
+    public override void OnInteractStart(Transform parent, Transform destination)
+    {
+        base.OnInteractStart(parent, destination);
+        TrailRenderer.enabled = false;
     }
     protected override void OnCollisionEnter(Collision collision)
     {
@@ -18,6 +26,7 @@ public class Ammo : Prop, IPlayerKillable
         {            
             transform.DOKill();
             RigidbodyObj.useGravity = true;
+            TrailRenderer.enabled = false;
             if (!collision.gameObject.CompareTag("Player")) IsDeadly = false;
         }
     }
