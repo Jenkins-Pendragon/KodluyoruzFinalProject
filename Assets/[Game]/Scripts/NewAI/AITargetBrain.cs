692ca14999bb7cdf5bd4d08216183327a5cc945c
Muhammed Üşenmez <mhusenmez@gmail.com>
2021-01-17 04:53:19 +0300

    Update prefabs

--- Assets/[Game]/Scripts/NewAI/AITargetBrain.cs
+++ Assets/[Game]/Scripts/NewAI/AITargetBrain.cs
@@ -13,7 +13,7 @@ namespace AICharacterController
 
         private Rigidbody rigidbody;
         public Rigidbody Rigidbody { get { return (rigidbody == null) ? rigidbody = GetComponent<Rigidbody>() : rigidbody; } }
-        public Transform targetPlayer;
+        
 
         private CharacterAnimationController characterAnimationController;
         public CharacterAnimationController CharacterAnimationController { get { return (characterAnimationController == null) ? characterAnimationController = GetComponent<CharacterAnimationController>() : characterAnimationController; } }
@@ -21,13 +21,13 @@ namespace AICharacterController
         
         public void Logic()
         {            
-            float distance = Vector3.Distance(targetPlayer.position, transform.position);
+            float distance = Vector3.Distance(PlayerData.Instance.transform.position, transform.position);
             if(distance < lookRadius)
             {
                 if (NavMeshAgent == null || NavMeshAgent.enabled == false)
                     return;
                 CharacterAnimationController.Run(true);
-                NavMeshAgent.SetDestination(targetPlayer.position);
+                NavMeshAgent.SetDestination(PlayerData.Instance.transform.position);
 
                 if (distance < NavMeshAgent.stoppingDistance)
                 {
